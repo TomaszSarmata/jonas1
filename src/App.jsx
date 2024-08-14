1447de5bc989411ea0aa80ed925e0446d97025da
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-08-14 08:48:40 +0100

    add getAdvice async func to display advice when button pressed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 export default function App() {
+  async function getAdvice() {
+    const res = await fetch('https://api.adviceslip.com/advice');
+    const data = await res.json();
+    console.log(data.slip.advice);
+  }
+
   return (
     <div>
       <h1>Hello World</h1>
-      <button>Get Advice</button>
+      <button onClick={getAdvice}>Get Advice</button>
     </div>
   );
 }
